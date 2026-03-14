a6bb764f9a54000ceb7d635207b712a68075f039
theognis1002 <theognis1002@gmail.com>
2026-03-14 18:59:49 -0400

    feat: dotenvy for env var loading

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -27,6 +27,8 @@ enum Commands {
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let _ = dotenvy::dotenv();
+
     tracing_subscriber::fmt()
         .with_env_filter(
             EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("warn")),
