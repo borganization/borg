95c761ff9617773f851e60c82e33e92398bf5ed7
theognis1002 <theognis1002@gmail.com>
2026-03-18 10:10:34 -0400

    feat: replace TUI logo with BORG Roman font ASCII art

--- crates/cli/src/logo.rs
+++ crates/cli/src/logo.rs
@@ -6,13 +6,13 @@ use std::io::{self, Write};
 
 #[allow(dead_code)]
 const LOGO: &str = r#"
-       ╭━━━━━━━╮
-      ╱ ●    ● ╲
-     │    ‿‿    │
-     │          │
-    ╱╲╱╲╱╲╱╲╱╲╱╲
-    ┃  tamago   ┃
-    ╰━━━━━━━━━━━╯
+oooooooooo.    .oooooo.   ooooooooo.     .oooooo.
+`888'   `Y8b  d8P'  `Y8b  `888   `Y88.  d8P'  `Y8b
+ 888     888 888      888  888   .d88' 888
+ 888oooo888' 888      888  888ooo88P'  888
+ 888    `88b 888      888  888`88b.    888     ooooo
+ 888    .88P `88b    d88'  888  `88b.  `88.    .88'
+o888bood8P'   `Y8bood8P'  o888o  o888o  `Y8bood8P'
 "#;
 
 #[allow(dead_code)]
